98e790c67823e3af39febda9f7cb462c0a2ea363
Knockturn <pkbrauner@gmail.com>
2022-02-21 20:11:30 +0100

    Array and slices started

---
+++ ArraysAndSlices/arraysAndSlices.go
@@ -0,0 +1,23 @@
+package main
+
+// Course part start: https://www.youtube.com/watch?v=YS4e4q9oBaU&t=6473s
+
+/*
+AGENDA:
+Arrays
+	Creation
+	Built-in functions
+	Working with arrays
+Slices
+	Creation
+	Built-in functions
+	Working with slices
+*/
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Print("")
+}
