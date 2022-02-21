918af43ef4cb6d4ae15a8fbe55d5976011341d2b
Philippe Kjellberg Bräuner <pkb@solar.dk>
2022-02-21 11:41:44 +0100

    add agenda

--- Primitives/primitives.go
+++ Primitives/primitives.go
@@ -1,5 +1,13 @@
 package main
 
+// Course part start: https://www.youtube.com/watch?v=YS4e4q9oBaU&t=3425s
+/*
+AGENDA:
+Boolean Types
+Numeric Types (Integers, Floating point, Complex numbers)
+Text Types
+*/
+
 func main() {
 
 }
