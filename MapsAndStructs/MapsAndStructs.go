0dd886e5e9bd9c701b2d198fecfa8975903c3530
Philippe Kjellberg Bräuner <pkb@solar.dk>
2022-02-23 11:36:28 +0100

    MapsAndStructs started

---
+++ MapsAndStructs/MapsAndStructs.go
@@ -0,0 +1,25 @@
+package main
+
+// Course part start: https://www.youtube.com/watch?v=YS4e4q9oBaU&t=8240s
+/*
+AGENDA:
+	Maps
+		What are they?
+		Creating
+		Manipulation
+	Structs
+		What are they?
+		Creating
+		Naming Convention
+		Embedding
+		Tags
+*/
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Print("Maps & Structs")
+	// Maps = Key Value pair.
+}
