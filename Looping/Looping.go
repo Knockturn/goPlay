76d410c2b56682bb1a0d2395cade09e0bad3bb0c
Philippe Kjellberg Bräuner <pkb@solar.dk>
2022-03-02 12:49:07 +0100

    progress in looping made

--- Looping/Looping.go
+++ Looping/Looping.go
@@ -18,4 +18,42 @@ func main() {
 	for i := 0; i < 5; i++ {
 		fmt.Println(i)
 	}
+	for i := 0; i < 5; i = i + 2 {
+		fmt.Println(i)
+	}
+	for i := 0; i < 5; {
+		fmt.Println(i)
+		i++ // incrementing inside loop
+	}
+
+	// Same as "while" loop below
+	i := 0
+	for i < 5 {
+		fmt.Println(i)
+		i++ // incrementing inside loop
+	}
+
+	// When to go until a special logic has been achieved inside loop (using break)
+	j := 0
+	for {
+		fmt.Println(j)
+		j++
+
+		if j == 5 {
+			break
+		}
+	}
+
+	// Initializing 2 vars in a for loop
+	for i, j := 0, 0; i < 5; i, j = i+1, j+2 {
+		fmt.Println(i, j)
+	}
+
+	/*
+	NOTES:
+	Best practice is to not mess with the counter inside the loop
+	*/
 }
+
+
+// PROGRESS: https://youtu.be/YS4e4q9oBaU?t=12685
