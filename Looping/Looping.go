90c888279c3873086211dd0e0028223a654fd30a
Philippe Kjellberg Bräuner <pkb@solar.dk>
2022-03-02 15:10:13 +0100

    looping finished

--- Looping/Looping.go
+++ Looping/Looping.go
@@ -27,6 +27,7 @@ func main() {
 	}
 
 	// Same as "while" loop below
+	fmt.Println("\nSame as While loop:")
 	i := 0
 	for i < 5 {
 		fmt.Println(i)
@@ -34,6 +35,7 @@ func main() {
 	}
 
 	// When to go until a special logic has been achieved inside loop (using break)
+	fmt.Println("\nBreak:")
 	j := 0
 	for {
 		fmt.Println(j)
@@ -44,16 +46,46 @@ func main() {
 		}
 	}
 
+	// Alternative for continue above is continue (continue is not used alot)
+	fmt.Println("\nContinue:")
+	for i := 0; i < 10; i++ {
+		if i%2 == 0 {
+			continue
+		}
+		fmt.Println(i)
+	}
+
 	// Initializing 2 vars in a for loop
+	fmt.Println("\nInitializing 2 vars")
 	for i, j := 0, 0; i < 5; i, j = i+1, j+2 {
 		fmt.Println(i, j)
 	}
 
+	// To break out of an inner loop and also break the outer loop: You can use a "label"
+	fmt.Println("\nInner Loop")
+	Loop:
+		for i := 1; i <= 3; i++ {
+			for j := 1; j <= 3; j++ { // inner loop
+				fmt.Println(i * j)
+				if i * j >= 3 {
+					break Loop
+				}
+			}
+		}
+
+	// looping through arbitrary(unkown) sized slice
+	fmt.Println("\nSlice")
+	s := []int{1, 2, 3}
+	for k, v := range s { // KEY, VALUE (useful perhaps in maps.Ex K&V: Texas 105687)
+		// if you only want value you can do this: "for _, v := range s" (and of course only k for key (k is just a representation - could be named anything))
+		fmt.Println(k,v)
+	}
+
+	/**/
+
 	/*
 	NOTES:
-	Best practice is to not mess with the counter inside the loop
+	Best practice is to not mess with the counter inside the loop.
+	For loops can be used for channels as well - but require special logic: check channels tutorial
 	*/
 }
-
-
-// PROGRESS: https://youtu.be/YS4e4q9oBaU?t=12685
