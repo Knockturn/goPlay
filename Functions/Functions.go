73b7345b9219275e6b43e449eb684736116e2265
Knockturn <pkbrauner@gmail.com>
2022-03-07 08:10:52 +0100

    function progress

--- Functions/Functions.go
+++ Functions/Functions.go
@@ -4,13 +4,32 @@ package main
 
 /*
 AGENDA:
-
+	Basix syntax
+	Parameters
+	Return values
+	Anonymous functions
+	Functions as types (first class citizen)
+	Method (special type of function)
 */
 
 import (
 	"fmt"
 )
 
-func main() {
+func main() { // This is the most basic function in go. As all of Go - capital start = global/shared
 	fmt.Println("Functions:")
+
+	sayMessage("Hello Go!")
+	sayGreeting("Hello", "Phi!")
 }
+
+// Parameters
+func sayMessage(msg string) {
+	fmt.Println(msg)
+}
+
+func sayGreeting(greeting, name string) { // Multiple parameters of same type can be done like this
+	fmt.Println(greeting, name)
+}
+
+// Progress: https://youtu.be/YS4e4q9oBaU?t=16081
