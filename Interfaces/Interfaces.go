59b781ddc916d0f04a906e3fdb19279367511b35
Knockturn <pkbrauner@gmail.com>
2022-03-08 13:59:55 +0100

    interfaces started

---
+++ Interfaces/Interfaces.go
@@ -0,0 +1,25 @@
+package main
+
+// YouTube start part: https://www.youtube.com/watch?v=YS4e4q9oBaU&t=17879s
+
+import (
+	"fmt"
+)
+
+/*
+Interfaces in Go are one of the reasons they are so scalable and maintanable.
+AGENDA:
+	Basics
+	Composing interfaces (can make interfaces of interfaces)
+	Type conversion (a bit different now with interfaces than previous tutorial)
+		The empty interface
+		Type switches
+	Implementing with values vs. pointers
+	Best practices
+*/
+
+func main() {
+	fmt.Println("Interfaces")
+}
+
+// Progress: https://youtu.be/YS4e4q9oBaU?t=17984
