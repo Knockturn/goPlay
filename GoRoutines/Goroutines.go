f8251152058fb3a19399738bafb9ddbca8f502ed
Knockturn <pkbrauner@gmail.com>
2022-03-15 20:27:48 +0100

    goroutines started

---
+++ GoRoutines/Goroutines.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+// YouTube Start: https://www.youtube.com/watch?v=YS4e4q9oBaU&t=20037s
+
+/*
+AGENDA (concurrent and parallel programming):
+	Creating goroutines
+	Synchronization (multiple goroutines together)
+		WaitGroups
+		Mutexes
+	Parallelism
+	Best practices
+*/
+
+func main() {
+	fmt.Println("Goroutines:")
+}
+
+// Progress: https://youtu.be/YS4e4q9oBaU?t=20155
