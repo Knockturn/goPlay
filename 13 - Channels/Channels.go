47243a72787761dee7453c2fcd99fff15606b0c8
Knockturn <pkbrauner@gmail.com>
2022-03-18 13:09:00 +0100

    updates and channels started

---
+++ 13 - Channels/Channels.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+// YouTube part start: https://www.youtube.com/watch?v=YS4e4q9oBaU&t=21910s
+
+/*
+Channels can be used to pass data between different goroutines
+in a safe way to avoid: race conditions, memory sharing problems.
+AGENDA:
+	- Channel basics
+	- Restricing data flow
+	- Buffered channels
+	- Closing channels
+	- For...range loops with channels
+	- Select statements (like switch statement but built for channels)
+*/
+
+func main() {
+	fmt.Println("Channels:")
+}
+
+// Progress: https://youtu.be/YS4e4q9oBaU?t=22060
