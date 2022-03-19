df25e0b33e7547cd755f957f45fb4afd2f6448ef
Knockturn <pkbrauner@gmail.com>
2022-03-19 13:37:20 +0100

    channels progress

--- 13 - Channels/Channels.go
+++ 13 - Channels/Channels.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 // YouTube part start: https://www.youtube.com/watch?v=YS4e4q9oBaU&t=21910s
 
@@ -16,8 +19,25 @@ AGENDA:
 	- Select statements (like switch statement but built for channels)
 */
 
+var wg = sync.WaitGroup{}
+
 func main() {
 	fmt.Println("Channels:")
-}
+	ch := make(chan int) // Strongly typed channel
+	wg.Add(2)
 
-// Progress: https://youtu.be/YS4e4q9oBaU?t=22060
+	go func() { // Recieving goroutine
+		i := <-ch // Pointing away from the channel because it pulls data FROM the channel
+		fmt.Println(i)
+		wg.Done()
+	}()
+
+	go func() { // Sending data to channel
+		nr := 42
+		ch <- nr // Arrow pointing to the way the data should flow (into the channel)
+		nr = 16  // Not a pointer so it will not affect that we change it now. The channel still recieves the copy of the data from when we set it (42)
+		wg.Done()
+	}()
+
+	wg.Wait()
+}
---
+++ 13 - Channels/Channels2.go
@@ -0,0 +1,11 @@
+package main
+
+import "sync"
+
+var wg = sync.WaitGroup{}
+
+func main() {
+	ch := make(chan int)
+}
+
+// PROGRESS: https://youtu.be/YS4e4q9oBaU?t=22314
