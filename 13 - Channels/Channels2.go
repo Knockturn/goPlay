631b37f458b526eb39dfef8383ae2ab4d78b2fa9
Knockturn <pkbrauner@gmail.com>
2022-03-20 15:07:15 +0100

    example done

--- 13 - Channels/Channels2.go
+++ 13 - Channels/Channels2.go
@@ -1,11 +1,28 @@
 package main
 
-import "sync"
+import (
+	"fmt"
+	"sync"
+)
 
 var wg = sync.WaitGroup{}
 
 func main() {
 	ch := make(chan int)
+	for j := 0; j < 5; j++ {
+		wg.Add(2)
+		go func() {
+			i := <-ch
+			fmt.Println(i)
+			wg.Done()
+		}()
+
+		go func() {
+			ch <- 42
+			wg.Done()
+		}()
+	}
+	wg.Wait()
 }
 
 // PROGRESS: https://youtu.be/YS4e4q9oBaU?t=22314
